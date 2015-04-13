74e305fab342a97de21a0685ef39a519f9359d98
Alexander Seipel <alexander.seipel@gmail.com>
2015-04-14 00:30:32 +0200

    Simple Movement
    Simple WASD movement and camera follow, no rotation yet

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    Vector3 offset;
+
+	void Start () {
+        offset = transform.position - target.position;
+	}
+	
+	void FixedUpdate () {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = targetCamPos;
+	}
+}
---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 3f;
+
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+
+	void Awake () {
+        playerRigidbody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Move(h, v);
+        Turn();
+	}
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        Debug.Log(movement);
+        movement = movement * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turn()
+    {
+        
+    }
+}
