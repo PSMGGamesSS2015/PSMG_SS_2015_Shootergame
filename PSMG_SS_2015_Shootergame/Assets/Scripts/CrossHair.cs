9e0b7c65670ab4f9853a2ba1ba927dff9f7dcd47
Alexander Seipel <alexander.seipel@gmail.com>
2015-04-19 13:58:50 +0200

    Basic Crosshair
    - implemented basic crosshair in the center of the screen
    - mouse cursor hides after the first mouse click - need to check if this
    is a bug or because it is just in the Unity preview
    - Debug message that texture is missing, but still works - need to check
    for bugs

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/CrossHair.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrossHair : MonoBehaviour {
+
+    public Texture2D crosshairTexture;
+    public float crosshairScale = 1;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void OnGUI()
+    {
+        Debug.Log(Cursor.visible);
+        //if not paused
+        if (Time.timeScale != 0)
+        {
+            if (crosshairTexture != null)
+                GUI.DrawTexture(new Rect((Screen.width - crosshairTexture.width * crosshairScale) / 2, (Screen.height - crosshairTexture.height * crosshairScale) / 2, crosshairTexture.width * crosshairScale, crosshairTexture.height * crosshairScale), crosshairTexture);
+            //else
+                //Debug.Log("No crosshair texture set in the Inspector");
+        }
+    }
+
+}
