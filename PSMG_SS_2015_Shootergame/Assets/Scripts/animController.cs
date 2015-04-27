731e5c8db72e5e4e4eb0559f78c54285bd382496
maischter <maischter@gmx.de>
2015-04-27 11:25:02 +0200

    idle and shot animation
    - added the setup/idel/shot-animation
    - transitions ane not that nice
    - sometimes the shot animation plays on fire, sometimes it does not

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/animController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animController : MonoBehaviour {
+
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire1")) {
+			anim.SetBool ("shot", true);
+		} else {
+			anim.SetBool("shot", false);
+		}
+	
+	}
+}
