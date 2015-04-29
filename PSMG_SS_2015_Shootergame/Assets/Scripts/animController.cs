42f68015e1c4536371a8224b2cc2717a12c2c281
maischter <maischter@gmx.de>
2015-04-29 17:15:03 +0200

    Basic movement animation and Armtextures
    - Arms now pretty pretty...
    - added 3 states of moving (idle, walk, run)

--- PSMG_SS_2015_Shootergame/Assets/Scripts/animController.cs
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/animController.cs
@@ -12,11 +12,21 @@ public class animController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton ("Fire1")) {
-			anim.SetBool ("shot", true);
-		} else {
-			anim.SetBool("shot", false);
+		if (Input.GetButton ("Vertical")) {
+			anim.SetBool ("walk", true);
+			if (Input.GetButton ("Sprint")) {
+				anim.SetBool("run", true);
+			}
+			else {
+				anim.SetBool("run", false);
+			}
 		}
-	
+
+
+
+		else {
+			anim.SetBool("walk", false);
+			anim.SetBool ("run", false);
+		}
 	}
 }
