efde1ceb878e55a81b4c0caa03b994467fec9786
LenaManschewski <lena.manschewski@stud.uni-regensburg.de>
2015-05-03 12:22:10 +0200

    renamed SetLifeMode Script to SetLifeVisualisation

--- PSMG_SS_2015_Shootergame/Assets/Scripts/SetLifeVisualisation.cs
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/SetLifeVisualisation.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class SetLifeMode : MonoBehaviour
+public class SetLifeVisualisation : MonoBehaviour
 {
     //The Player Object
     public PlayerMovement movement;
