317703b55123a4b03412218e0e6c420595b70f34
LenaManschewski <lena.manschewski@stud.uni-regensburg.de>
2015-05-21 18:35:42 +0200

    animated human and bird icons (prototype)

--- PSMG_SS_2015_Shootergame/Assets/UiRotation.cs
+++ PSMG_SS_2015_Shootergame/Assets/UiRotation.cs
@@ -3,7 +3,13 @@ using System.Collections;
 
 public class UiRotation : MonoBehaviour
 {
+    public PlayerMovement movement;
+
     public Transform redHealthBar;
+    public Transform blueFlapBar;
+    public Transform panel1;
+    public Transform panel2;
+
     /// <summary>
     /// The time taken to move from the start to finish positions
     /// </summary>
@@ -18,15 +24,20 @@ public class UiRotation : MonoBehaviour
     private bool _isLerping;
  
     //The start and finish positions for the interpolation
-    private Vector3 _startPosition;
-    private Vector3 _endPosition;
+    private Vector3 _startPositionRed;
+    private Vector3 _endPositionRed;
+    private Vector3 _startPositionBlue;
+    private Vector3 _endPositionBlue;
  
     //The Time.time value when we started the interpolation
     private float _timeStartedLerping;
-    
+
+    private Vector3 birdVector;
+    private Vector3 humanVector;
+
     void Start()
     {
-        GameObject.FindGameObjectsWithTag("LifeVisualisation Flap");
+        //flapObjects = GameObject.FindGameObjectsWithTag("LifeVisualisation Flap");
     }
 
     /// <summary>
@@ -34,12 +45,15 @@ public class UiRotation : MonoBehaviour
     /// </summary>
     void StartLerping()
     {
+
         _isLerping = true;
         _timeStartedLerping = Time.time;
  
         //We set the start position to the current position, and the finish to 10 spaces in the 'forward' direction
-        _startPosition = redHealthBar.position;
-        _endPosition = redHealthBar.position - new Vector3(60f, 50f, 0f);
+        _startPositionRed = redHealthBar.transform.position;
+        _endPositionRed = redHealthBar.transform.position - new Vector3(75f, 60f, 0f);
+        _startPositionBlue = blueFlapBar.transform.position;
+        _endPositionBlue = blueFlapBar.transform.position + new Vector3(50f, 37f, 0f);
     }
  
     void Update()
@@ -47,6 +61,17 @@ public class UiRotation : MonoBehaviour
         //When the user hits the spacebar, we start lerping
         if(Input.GetKey(KeyCode.F10))
         {
+            if (movement.getMode())
+            {
+                birdVector = new Vector3(0.3f, 0.3f, 0.3f);
+                humanVector = new Vector3(2f, 2f, 2f);
+            }
+            else
+            {
+                birdVector = new Vector3(2f, 2f, 2f);
+                humanVector = new Vector3(0.3f, 0.3f, 0.3f);
+            }
+
             StartLerping();
         }
     }
@@ -66,8 +91,13 @@ public class UiRotation : MonoBehaviour
             //Perform the actual lerping.  Notice that the first two parameters will always be the same
             //throughout a single lerp-processs (ie. they won't change until we hit the space-bar again
             //to start another lerp)
-            redHealthBar.position = Vector3.Lerp(_startPosition, _endPosition, percentageComplete);
-            redHealthBar.transform.localScale = Vector3.Lerp(redHealthBar.localScale, new Vector3(5f, 5f, 5f), Time.deltaTime);
+            //panel1.transform.position = Vector3.Lerp(_startPosition, _endPosition, percentageComplete);
+            redHealthBar.transform.position = Vector3.Lerp(_startPositionRed, _endPositionRed, percentageComplete);
+            redHealthBar.transform.localScale = Vector3.Lerp(redHealthBar.transform.localScale, new Vector3(5f, 5f, 5f), Time.deltaTime);
+            blueFlapBar.transform.position = Vector3.Lerp(_startPositionBlue, _endPositionBlue, percentageComplete);
+            blueFlapBar.transform.localScale = Vector3.Lerp(blueFlapBar.transform.localScale, new Vector3(10.0f, 10.0f, 10.0f), Time.deltaTime);
+            panel1.transform.localScale = Vector3.Lerp(panel1.transform.localScale, humanVector, Time.deltaTime);
+            panel2.transform.localScale = Vector3.Lerp(panel2.transform.localScale, birdVector, Time.deltaTime);
             
             //When we've completed the lerp, we set _isLerping to false
             if(percentageComplete >= 1.0f)
