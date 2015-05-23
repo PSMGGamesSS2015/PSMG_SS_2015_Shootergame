b852335a947ff6e721dbfda801a6a6e8b73357e5
Alexander Seipel <alexander.seipel@gmail.com>
2015-05-23 16:23:48 +0200

    Simple Follow Quest Script
    Implemented a basic script for a following quest

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/QuestFollow.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestFollow : MonoBehaviour {
+
+    /// <summary>
+    /// The object that needs to remain within range of the target - usually the plyaer
+    /// </summary>
+    public Transform player;
+
+    /// <summary>
+    /// The trigger that will start the quest - the quest will start if the player is within a set distance of the trigger
+    /// </summary>
+    public Transform trigger;
+
+    /// <summary>
+    /// The goal that needs to be reached for the quest to finish
+    /// </summary>
+    public Transform goal;
+
+    public enum FinishTrigger { Player = 0, Target = 1 }
+    /// <summary>
+    /// Choose if the player or the target have to be within range of the goal to finish the quest
+    /// </summary>
+    public FinishTrigger finishTrigger = FinishTrigger.Player;
+
+    /// <summary>
+    /// Distance to the start trigger to start the quest
+    /// </summary>
+    public float startDistance = 50.0f;
+    /// <summary>
+    /// The player needs to remain within this distance to the target in order for the quest to not fail
+    /// </summary>
+    public float failDistance = 50.0f;
+    /// <summary>
+    /// Distance the finish trigger (set above) needs to have to the goal in order for the quest to finish
+    /// </summary>
+    public float goalDistance = 20.0f;
+
+    private Transform finishTriggerObject;
+    private bool questStarted = false;
+    private bool questFinished = false;
+
+	// Use this for initialization
+	void Start () {
+        if (finishTrigger == FinishTrigger.Player) {
+            finishTriggerObject = player;
+        } else if (finishTrigger == FinishTrigger.Target) {
+            finishTriggerObject = transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (!questStarted)
+        {
+            CheckForQuestStart();
+        }
+        else
+        {
+            CheckFail();
+            CheckFinish();
+        }
+	}
+
+    void CheckForQuestStart()
+    {
+        float distance = Vector3.Distance(player.position, trigger.position);
+
+        if (distance <= startDistance)
+        {
+            StartQuest();
+        }
+    }
+
+    void CheckFail()
+    {
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if (distance >= failDistance)
+        {
+            QuestFailed();
+        }
+    }
+
+    void CheckFinish()
+    {
+        float distance = Vector3.Distance(finishTriggerObject.position, transform.position);
+        if (distance <= goalDistance)
+        {
+            Debug.Log("Quest finished!");
+            questStarted = false;
+            questFinished = true;
+        }
+    }
+
+    void StartQuest() 
+    {
+        Debug.Log("Quest started!");
+        questStarted = true;
+    }
+
+    void QuestFailed()
+    {
+        questStarted = false;
+        Debug.Log("Quest has failed!");
+    }
+}
