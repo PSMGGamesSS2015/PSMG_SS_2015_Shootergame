0867aaa1da9219ebb430cfa91907d937c7438b46
Alexander Seipel <alexander.seipel@gmail.com>
2015-06-08 17:55:45 +0200

    Basic Movement Controller
    Not yet implemented; goal:
    - communicate between movement scripts to ensure that only one is
    running at a time
    - switch which script is active

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/MovementController.cs
@@ -0,0 +1,36 @@
+﻿/* !!!!!!!!!!!!!!!
+ * !!!!!!!!!!!!!!!
+ * NOT WORKING YET
+ * NOT WORKING YET
+ * !!!!!!!!!!!!!!!
+ !!!!!!!!!!!!!!!*/
+
+using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+
+    private WaypointMovement waypointMovement;
+    private Enemy enemy;
+
+	// Use this for initialization
+	void Start () {
+        waypointMovement = GetComponent<WaypointMovement>();
+        enemy = GetComponent<Enemy>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void followPlayer()
+    {
+        // waypointMovement.StopMoving();
+    }
+
+    public void goHome()
+    {
+        // waypointMovement.GoHome();
+    }
+}
