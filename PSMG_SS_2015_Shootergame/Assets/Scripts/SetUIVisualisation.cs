a7afc253ae0b07f3c8ebd18e8865f6609af386ce
LenaManschewski <lena.manschewski@stud.uni-regensburg.de>
2015-06-08 18:34:15 +0200

    Change Text on UI
    - Infotext Customisation
    - Infotext einfaden/ausfaden
    - Infotext weiter nach oben verschieben, nicht vertikal zentriert
    -Crosshair in UI

--- PSMG_SS_2015_Shootergame/Assets/Scripts/SetUIVisualisation.cs
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/SetUIVisualisation.cs
@@ -80,10 +80,17 @@ public class SetUIVisualisation : MonoBehaviour
     //Tutorial text
     public Text textfield;
 
+    //the time the text should be shown
+    private float timeToShowText = 5f;
+
+    //Time to fade text in or out
+    private float textFadeDuration = 1.0f;
+
     // Use this for initialization
     void Start()
     {
         //showTextinUI("Test text zum schaun wie das alles aussieht! ", 5);
+        //showTextinUI("Test text zum schaun wie das alles aussieht! ", 5, Color.red, 50);
         smallVector = new Vector3(0.6f, 0.6f, 0.6f);
         bigVector = new Vector3(1.0f, 1.0f, 1.0f);
 
@@ -210,18 +217,65 @@ public class SetUIVisualisation : MonoBehaviour
         return isModeChanged;
     }
 
-    //show the text for a specific time at the ui and then disable it
+    //show the text for a specific time at the ui and then fade it out
     public void showTextinUI(string text, float time)
     {
         textfield.enabled = true;
         textfield.text = text;
-        Invoke("disableUIText", time);
+        FadeIn();
+        FadeOut();
     }
 
-    //disable the text on the UI
-    private void disableUIText()
+    //show the text for a specific time at the ui and then fade it out
+    public void showTextinUI(string text, float time, Color textColor, int textSize)
     {
-        textfield.enabled = false;
+        textfield.enabled = true;
+        textfield.text = text;
+        textfield.color = textColor;
+        textfield.fontSize = textSize;
+
+        FadeIn();
+        FadeOut();
     }
-        
+
+    //Fade the text in
+    public void FadeIn()
+    {
+        StartCoroutine("FadeInCR");
+    }
+
+    //Coroutine to fade the text in
+    private IEnumerator FadeInCR()
+    {
+        float currentTime = 0f;
+        while (currentTime < textFadeDuration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, currentTime / textFadeDuration);
+            textfield.color = new Color(textfield.color.r, textfield.color.g, textfield.color.b, 1 - alpha);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        yield break;
+    }
+
+    //fade the text out
+    public void FadeOut()
+    {
+        StartCoroutine("FadeOutCR");
+    }
+
+    //Coroutine to fade the text out (wait for timeToShowText so the text is visible this amount of time
+    private IEnumerator FadeOutCR()
+    {
+        float currentTime = 0f;
+        yield return new WaitForSeconds(timeToShowText);
+        while (currentTime < textFadeDuration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, currentTime / textFadeDuration);
+            textfield.color = new Color(textfield.color.r, textfield.color.g, textfield.color.b, alpha);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        yield break;
+    }
 }
