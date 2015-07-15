9ecd63b799caa7ef57917f4ca21b0f526634d564
mark-engerisser <mark.engerisser@stud.uni-regensburg.de>
2015-07-15 03:14:41 +0200

    Feature to cut down that specified tree in the rocks. Closes #98

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/TreeCutDown.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Weapons;
+
+public class TreeCutDown : MonoBehaviour {
+
+    private int counter = 0;
+    private Animation anim;
+
+    private bool isFallen = false;
+
+    private void onTomahawkAttack(BaseWeapon w)
+    {
+        if (isFallen) return;
+
+        float distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+        if (distance <= 5.5f)
+        {
+            counter++;
+
+            if (counter >= 4)
+            {
+                anim.Play();
+                isFallen = true;
+            }
+        }
+    }
+
+    private bool initialized = false;
+    void FixedUpdate()
+    {
+        if (initialized) return;
+        WeaponController wpc = player.GetComponent<WeaponController>();
+        BaseWeapon tomahawk = wpc.getWeaponByName("Tomahawk");
+        tomahawk.OnShotFired += new BaseWeapon.DOnWeaponInfoChanged(onTomahawkAttack);
+        initialized = true;
+    }
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+        anim = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
