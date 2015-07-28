e2593954fe74c97ac65afe6651edfff09f266cfe
LenaManschewski <lena.manschewski@stud.uni-regensburg.de>
2015-07-28 23:40:07 +0200

    Added first Menue prototyp
    -weiterleitung zu "philippsSpielplatz"
    -ladescreen
    -Playermovement in "philippsSpielplatz" gefixt

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/MenueSelector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenueSelector : MonoBehaviour {
+    public GameObject loadingImage;
+    
+        
+    public void LoadScene(int sceneNumber)
+    {
+        loadingImage.SetActive(true);
+        Application.LoadLevel(sceneNumber);
+    }
+}
