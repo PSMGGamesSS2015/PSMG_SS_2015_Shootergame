3e1fcc317bc9bd7fab8caf7aef3c58c08f8b01c2
LenaManschewski <lena.manschewski@stud.uni-regensburg.de>
2015-07-31 00:32:22 +0200

    Load Level Async
    -funktioniert noch nicht richtig

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/ClickToLoadLevelAsync.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClickToLoadLevelAsync : MonoBehaviour
+{
+    public Slider loadingBar;
+    public GameObject loadingImage;
+
+    private AsyncOperation async;
+
+    public void ClickAsync(int level)
+    {
+        loadingImage.SetActive(true);
+        StartCoroutine(LoadLevelWithBar(level));
+    }
+
+    IEnumerator LoadLevelWithBar(int level)
+    {
+        async = Application.LoadLevelAsync(level);
+        while (!async.isDone)
+        {
+            //Debug.Log(async.progress);
+            loadingBar.value = async.progress;
+            //Debug.Log(loadingBar.value);
+            yield return null;
+        }
+    }
+}
