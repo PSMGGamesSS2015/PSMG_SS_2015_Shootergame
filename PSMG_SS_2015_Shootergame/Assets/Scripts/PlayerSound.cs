78ba6afd0594ebfd8234b46106fa6202040b9935
KevinMyers <KKastner@gmx.net>
2015-08-11 12:33:59 +0200

    Player Movement Sound Script
    basic external script to manage player movement sounds

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/PlayerSound.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSound : MonoBehaviour {
+
+    public AudioClip thud;
+
+    private AudioSource aSource;
+
+    private float soundDelay = 0;
+
+    private bool sprinting = false;
+
+    private bool crouching = false;
+
+    private bool sneaking = false;
+
+    private bool moving = false;
+
+    private int test = 0;
+
+	// Use this for initialization
+	void Start () {
+        updateMovement();
+        aSource = GetComponent<AudioSource>();
+	}
+
+    void Awake()
+    {
+        
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        updateMovement();
+
+        if (moving && !sprinting && !crouching)
+        {
+            playMoveSound(0.05F, 0.1F);
+        }
+        else if (sprinting)
+        {
+            playMoveSound(-0.1F, 1F);
+        }
+        else if (moving && crouching)
+        {
+            playMoveSound(0.1F, 0.03F);
+        }
+
+
+
+	}
+
+    private void updateMovement()
+    {
+
+        moving = GetComponent<PlayerMovement>().moving;
+        crouching = GetComponent<PlayerMovement>().crouching;
+        sprinting = GetComponent<PlayerMovement>().sprinting;
+        sneaking = GetComponent<PlayerMovement>().sneaking;
+        test = GetComponent<PlayerMovement>().test;
+
+
+    }
+
+    private void playMoveSound(float delay, float vol)
+    {
+
+        if (Time.time >= soundDelay)
+        {
+            aSource.PlayOneShot(thud, vol);
+            soundDelay = Time.time + thud.length-0.7F + delay;
+        }
+
+    }
+}
