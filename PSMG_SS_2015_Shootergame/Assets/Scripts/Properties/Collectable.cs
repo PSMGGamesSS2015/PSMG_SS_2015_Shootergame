047c1810c0375571686396548c608a3c77b1825a
Alexander Seipel <alexander.seipel@gmail.com>
2015-08-12 12:00:23 +0200

    Collectables + Collectable Quest
    - objects can now have collectable as property
    - implemented quest where the player has to collect objects
    - shoot quest script updated

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/Properties/Collectable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class Collectable : MonoBehaviour {
+
+    private bool collected = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            collected = true;
+            gameObject.SetActive(false);
+        }
+    }
+
+    public bool IsCollected()
+    {
+        return collected;
+    }
+
+    public void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
--- PSMG_SS_2015_Shootergame/Assets/Scripts/Quests/QuestCollect.cs
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/Quests/QuestCollect.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class QuestShoot : Quest {
+public class QuestCollect : Quest {
 
-    public Shootable[] targets;
+    public Collectable[] targets;
 
-    private ArrayList toShoot;
-    private ArrayList shot;
+    private ArrayList toCollect;
+    private ArrayList collected;
 
     private float progress;
 
@@ -16,13 +16,13 @@ public class QuestShoot : Quest {
 
     protected override void OnStart()
     {
-        toShoot = new ArrayList();
-        shot = new ArrayList();
+        toCollect = new ArrayList();
+        collected = new ArrayList();
         progress = 0.0f;
-
+        
         for (int i = 0; i < targets.Length; i++)
         {
-            toShoot.Add(targets[i]);
+            toCollect.Add(targets[i]);
         }
     }
 	
@@ -37,20 +37,21 @@ public class QuestShoot : Quest {
 
     void CheckTargets()
     {
-        for (int i = 0; i < toShoot.Count; i++) {
-            if (((Shootable)toShoot[i]).GetHealth() == 0.0f)
+        for (int i = 0; i < toCollect.Count; i++)
+        {
+            if (((Collectable)toCollect[i]).IsCollected())
             {
-                TargetShot(i);
+                TargetCollected(i);
             }
         }
     }
 
-    void TargetShot(int index)
+    void TargetCollected(int index)
     {
-        shot.Add(toShoot[index]);
-        toShoot.RemoveAt(index);
+        collected.Add(toCollect[index]);
+        toCollect.RemoveAt(index);
 
-        progress = (float) shot.Count / (toShoot.Count + shot.Count);
+        progress = (float) collected.Count / (toCollect.Count + collected.Count);
     }
 
     protected override void CheckFinish()
