f9d19ef5f1d6d125f0bbfed5091d33de291b13cc
Alexander Seipel <alexander.seipel@gmail.com>
2015-08-12 16:29:55 +0200

    Totem and TotemQuest updated
    not 100% finished yet

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/Properties/BonusTotem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonusTotem : MonoBehaviour {
+
+    public float activatingTime = 5.0f;
+
+    public float activationRange = 10.0f;
+
+    public bool showIndicator = false;
+
+    private bool active = false;
+    private float activationState = 0.0f;
+    private float activeTime = 0.0f;
+
+    private Transform player;
+    private PrefabManager prefabs;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        prefabs = GameObject.FindGameObjectWithTag("Prefabs").GetComponent<PrefabManager>();
+
+        if (showIndicator)
+        {
+            GameObject marker = Instantiate(prefabs.rangeIndicator, transform.position, Quaternion.Euler(90, 0, 0)) as GameObject;
+            marker.transform.parent = transform;
+            marker.GetComponent<Projector>().orthographicSize = activationRange;
+            marker.GetComponentInChildren<ParticleSystem>().Stop();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
