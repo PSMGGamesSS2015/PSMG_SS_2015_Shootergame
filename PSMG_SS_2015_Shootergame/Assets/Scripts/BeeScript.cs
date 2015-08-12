f8196c3454b32afa3450f8797090b45c62124087
Alexander Seipel <alexander.seipel@gmail.com>
2015-08-12 10:50:01 +0200

    Updated BeeScript
    "Shootable" is now a property that can be added to any game object with
    a trigger

--- PSMG_SS_2015_Shootergame/Assets/Scripts/BeeScript.cs
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/BeeScript.cs
@@ -1,28 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Shootable))]
 public class BeeScript : MonoBehaviour {
 
+    public float playerDistance = 4.0f;
+    public int damage = 1;
+
 	private GameObject player;
-
-	void OnTriggerEnter(Collider other) {
-		if(other.gameObject.tag == "Arrow") {
-			GetComponent<Rigidbody> ().useGravity = true;
-			GetComponent<ParticleSystem>().Play();
-		}
-	}
-
+    private Shootable shootable;
+    
 	void Start() {
 		player = GameObject.FindGameObjectWithTag ("Player");
-
+        shootable = GetComponent<Shootable>();
 	}
 
 	void Update() {
-		if (Vector3.Distance (player.transform.position, transform.position) <= 4.0f) {
-			player.GetComponent<BasePlayer> ().SubtractHealth (1);
+		if (Vector3.Distance (player.transform.position, transform.position) <= playerDistance) {
+			player.GetComponent<BasePlayer> ().SubtractHealth (damage);
 		} else {
 			//bahahahahaha
 		}
+        if (shootable.getHealth() == 0.0f)
+        {
+            GetComponent<ParticleSystem>().Play();
+        }
 	}
 
 }
---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/Properties/Shootable.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class Shootable : MonoBehaviour {
+
+    public int shotsNeeded = 3;
+    public bool destroyAfterShot = false;
+    public bool fallAfterShot = false;
+
+    private GameObject player;
+    private int health;
+    private float pct;
+
+	// Use this for initialization
+	void Start () {
+        health = shotsNeeded;
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (fallAfterShot)
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+        }
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger enter!");
+        if (other.gameObject.tag == "Arrow")
+        {
+
+            health--;
+            health = health / shotsNeeded;
+
+            if (health == 0)
+            {
+                checkEffects();
+            }
+        }
+    }
+
+    void checkEffects()
+    {
+        CheckFall();
+        CheckDestroy();
+    }
+
+    void CheckFall()
+    {
+        if (fallAfterShot)
+        {
+            Debug.Log("falling now!");
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    void CheckDestroy()
+    {
+        if (destroyAfterShot)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public float getHealth()
+    {
+        return pct;
+    }
+}
