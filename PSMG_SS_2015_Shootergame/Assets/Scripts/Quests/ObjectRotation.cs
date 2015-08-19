c7aec44118598efae05ab517cebc1b8bb9e8c575
maischter <philipp.stefan.mai@gmail.com>
2015-08-19 15:35:23 +0200

    RotationScript added
    also minor stuff

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/Quests/ObjectRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRotation : MonoBehaviour {
+
+    public float xRotationSpeed;
+    public float yRotationSpeed;
+    public float zRotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(xRotationSpeed * Time.deltaTime, yRotationSpeed * Time.deltaTime, zRotationSpeed * Time.deltaTime);
+	
+	}
+}
