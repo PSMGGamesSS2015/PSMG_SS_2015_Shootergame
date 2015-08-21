abc0854f8981639165d3c83e00e0e0dfac1eff39
Alexander Seipel <alexander.seipel@gmail.com>
2015-08-21 15:12:12 +0200

    Follow script + Script folder structure

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/Enemy/FollowMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class FollowMovement : MonoBehaviour {
+
+	public Transform target;
+	public float minDistance = 5.0f;
+
+	private bool moving = true;
+
+	private NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+		nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (moving) {
+			if (Vector3.Distance(transform.position, target.position) >= minDistance) {
+				nav.SetDestination(target.position);
+			}
+		}
+	}
+
+	public void StartMoving()
+	{
+		moving = true;
+	}
+	
+	public void StopMoving()
+	{
+		moving = false;
+	}
+}
---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/Enemy/MovementController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+
+    private WaypointMovement waypointMovement;
+	private FollowMovement followMovement;
+    private Enemy enemy;
+
+    private Vector3 savedPosition;
+
+	private string mode;
+
+	// Use this for initialization
+	void Start () {
+        waypointMovement = GetComponent<WaypointMovement>();
+		followMovement = GetComponent<FollowMovement> ();
+
+		if (waypointMovement == null) {
+			mode = "follow";
+		} else if (followMovement == null) {
+			mode = "waypoint";
+		}
+
+        enemy = GetComponent<Enemy>();
+	}
+
+    public void OnFollowPlayer()
+    {
+        savedPosition = transform.position;
+		StopMoving ();
+    }
+
+	void StopMoving() {
+		if (mode == "follow") {
+			followMovement.StopMoving ();
+		} else if (mode == "waypoint") {
+			waypointMovement.StopMoving ();
+		}
+	}
+
+    public void OnStopFollowing()
+    {
+		if (mode == "follow") {
+			followMovement.StartMoving ();
+		} else if (mode == "waypoint") {
+			waypointMovement.StartMoving ();
+		}
+    }
+}
