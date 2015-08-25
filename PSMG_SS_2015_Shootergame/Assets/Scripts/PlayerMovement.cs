1d1d0a8099c0f21fa2759ec28769209d0da6c954
Alexander Seipel <alexander.seipel@gmail.com>
2015-08-25 15:22:23 +0200

    Small adjustment

--- PSMG_SS_2015_Shootergame/Assets/Scripts/PlayerMovement.cs
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/PlayerMovement.cs
@@ -247,7 +247,7 @@ public class PlayerMovement : MonoBehaviour
     void CheckForFlyMode()
     {
         // If the button for activation of fly mode is pressed...
-        if (!flyModeActivated && Input.GetButton("Fly") && GetComponent<BasePlayer>().getCurrentFeathers() >= 1)
+        if (!flyModeActivated && Input.GetButton("Fly") && basePlayer.getCurrentFeathers() >= 1)
         {
             StartCoroutine(startFlyingThread());
         }
