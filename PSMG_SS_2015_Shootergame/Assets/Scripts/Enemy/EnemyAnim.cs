19636458f091d82d9d0e68a566493a3580737351
maischter <philipp.stefan.mai@gmail.com>
2015-08-25 16:17:24 +0200

    Enemy can now move or idle
    idle + walk + sprint eingebaut. funktioniert abhänig davon, wie schnell
    sich die figur bewegt

---
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/Enemy/EnemyAnim.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAnim : MonoBehaviour {
+
+    private float speed;
+    private Animator anim;
+    private Vector3 prev;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        speed = ((transform.position - prev).magnitude) / Time.deltaTime;
+        prev = transform.position;
+        anim.SetFloat("Speed", speed);
+	}
+}
