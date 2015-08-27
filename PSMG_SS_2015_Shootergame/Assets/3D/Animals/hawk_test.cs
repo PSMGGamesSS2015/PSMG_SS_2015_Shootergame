c3a3b5b59cdc26730cde223b52700e9750982a12
maischter <maischter@gmx.de>
2015-08-27 02:13:16 +0200

    Added an animated hawk

---
+++ PSMG_SS_2015_Shootergame/Assets/3D/Animals/hawk_test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hawk_test : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		anim.SetBool ("Flap", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
