7a0aa4861cd22d1f5002bfafe0f51869dfee8d38
KevinMyers <KKastner@gmx.net>
2015-08-30 20:05:49 +0200

    Fixed jump sound, added creek water noise to kscene

--- PSMG_SS_2015_Shootergame/Assets/Scripts/PlayerMovement.cs
+++ PSMG_SS_2015_Shootergame/Assets/Scripts/PlayerMovement.cs
@@ -340,22 +340,17 @@ public class PlayerMovement : MonoBehaviour
         else
         {
 			float fallDistance = groundHeight - transform.position.y;
-
             if (fallDistance >= fallDamageMinDistance)
             {
 				groundHeight = transform.position.y;
                 int damage = (int) Mathf.Round(Mathf.Pow((fallDistance / 4), 1.8f));
                 basePlayer.SubtractHealth(damage);
-
-				if (inAir && fallDistance < 0.4F)
-				{
-					inAir = false;
-					audioController.playGround();
-				}
-				if (fallDistance >= 2F)
-				{
-					audioController.playGround();
-				}
+                audioController.playGround();
+            }
+            if (inAir && fallDistance < 0.4F)
+            {
+                inAir = false;
+                audioController.playGround();
             }
         }
     }
